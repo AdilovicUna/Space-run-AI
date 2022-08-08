d1b24621dee4ac43d5c8419c73d949be9573bdff
Una Adilović <adilovicuna@gmail.com>
2022-08-08 19:43:12 +0200

    plots added

--- Plots/plot.py
+++ Plots/plot.py
@@ -1,26 +1,36 @@
 
 import matplotlib.pyplot as plt 
 import numpy as np
+import os
+from pathlib import Path
 
-filename = "agent_MonteCarlo_tunnel_1_sorted_env_[Triangles]_shooting_True_actions_[[-1,0],[0,0],[1,0]]_dists_4_rots_12"
-f = open("../Game/Command_outputs/" + filename + ".txt", "r")
-data = f.read().strip().split('\n')
+'''
+loop through all the files from Command_outputs, plot the data and save the plot (unless it already exists)
+'''
+def main():
+    path = "../Game/Command_outputs/"
+    for filename in os.listdir(path):
+        filename = filename[:-4]
+        f = open("../Game/Command_outputs/" + filename + ".txt", "r")
+        data = f.read().strip().split('\n')
+        plot(data)
+        plt.savefig(filename + ".png")
 
-window = 10
-scores = [float(i) for i in data[:-1]]
-scores = [np.mean(scores[i:i + window]) if i <= len(scores) - window else np.mean(scores[i:]) for i in range(len(scores))]
-episodes = [i for i in range(1,len(scores)+1)]
-avg_score = [float(data[-1])] * len(scores)
+      
+def plot(data):
+    window = 10
+    scores = [float(i) for i in data[:-1]]
+    scores = [np.mean(scores[i:i + window]) if i <= len(scores) - window else np.mean(scores[i:]) for i in range(len(scores))]
+    episodes = [i for i in range(1,len(scores)+1)]
+    avg_score = [float(data[-1])] * len(scores)
 
-fig,ax = plt.subplots()cd 
+    _,ax = plt.subplots()
 
-data_line = ax.plot(episodes, scores, label='Data')
-mean_line = ax.plot(episodes, avg_score, label='Mean', linestyle='--')
-legend = ax.legend(loc='upper right')
+    ax.plot(episodes, scores, label='Data')
+    ax.plot(episodes, avg_score, label='Mean', linestyle='--')
+    ax.legend(loc='upper right')
 
-plt.xlabel('Episodes') 
-plt.ylabel('Scores') 
-    
-plt.title(filename) 
+    plt.xlabel('Episodes') 
+    plt.ylabel('Scores') 
 
-plt.show()
+main()
