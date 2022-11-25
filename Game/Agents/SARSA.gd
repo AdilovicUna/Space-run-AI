6cac12a2aa35fb8c7fd106421aa61e81c24d6c67
Una Adilović <adilovicuna@gmail.com>
2022-11-25 17:21:11 +0100

    small fix

--- Game/Agents/SARSA.gd
+++ Game/Agents/SARSA.gd
@@ -115,8 +115,7 @@ func init(actions, read, write, filename, curr_n, debug):
             else:
                 line = line.split(':')
                 q[line[0]] = float(line[1])
-                #visits[line[0]] = float(line[2])
-                visits[line[0]] = 0
+                visits[line[0]] = float(line[2])
                
         file.close()
     else:
